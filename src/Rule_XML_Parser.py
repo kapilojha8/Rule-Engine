59b25d023e567fd7479d8d9a1ca34b06e4aece44
Ritik Raghuwanshi <ritik.raghuwanshi@mindruby.com>
2024-10-14 16:00:02 +0530

    Xml parser code completed and run on few records

--- src/Rule_XML_Parser.py
+++ src/Rule_XML_Parser.py
@@ -15,69 +15,6 @@ from datetime import datetime
             The rules are connected in a linked list manner using the Rule_Connection class.
             Each lender's rules are stored in a dictionary (lender_rules), with the lender's name as the key.
 """
-
-
-def Logical_OR_NestedRule(rule_var, rule_key="",parent_rule_heading=""):
-    """
-        Recursively creates Rule objects based on the provided rule data. Handles nested and logical rules.
-
-        Args:
-            rule_var (dict): Dictionary containing rule information.
-            rule_key (str): Unique identifier for the rule.
-            parent_rule_heading (str): The heading of the parent rule, used for reference.
-
-        Returns:
-            Rule: A Rule object created based on the input rule data.
-    """
-    try:
-        Flow_exception_True_Flow_rule, Flow_exception_True_Condition_to_proceed,Flow_exception_True_Remark, Flow_exception_False_Flow_rule, Flow_exception_False_Condition_to_proceed,Flow_exception_False_Remark = None, None, None, None, None, None
-        if rule_var.get('Flow_Exception_for_True'):
-            if rule_var.get('Flow_Exception_for_True').get('Exception_rule'):
-                Flow_exception_True_Flow_rule = rule_var['Flow_Exception_for_True'].get('Exception_rule')
-            if rule_var.get('Flow_Exception_for_True').get('Condition_to_proceed'): 
-                Flow_exception_True_Condition_to_proceed = rule_var['Flow_Exception_for_True'].get('Condition_to_proceed')
-            if rule_var.get('Flow_Exception_for_True').get('Remark'): 
-                Flow_exception_True_Remark = rule_var['Flow_Exception_for_True'].get('Remark')
-
-        if rule_var.get('Flow_Exception_for_False'):
-            if rule_var.get('Flow_Exception_for_False').get('Exception_rule'):
-                Flow_exception_False_Flow_rule = rule_var['Flow_Exception_for_False'].get('Exception_rule')
-            if rule_var.get('Flow_Exception_for_False').get('Condition_to_proceed'): 
-                Flow_exception_False_Condition_to_proceed = rule_var['Flow_Exception_for_False'].get('Condition_to_proceed')
-            if rule_var.get('Flow_Exception_for_False').get('Remark'): 
-                Flow_exception_False_Remark = rule_var['Flow_Exception_for_False'].get('Remark')
-
-        return Rule(
-                ID=rule_key,
-                Rule_header    =  rule_var['Reference_field'],
-                Rule_operator  =  rule_var['Rule_Operator'],
-                Rule_value     =  rule_var['Rule_Value'],
-                Field_Type     =  rule_var.get('Field_Type'),            
-                Is_Nested      =  rule_var.get('Is_Nested', False),
-                Nested_Rule    =  Logical_OR_NestedRule(rule_var['Nested_Rule'],"Nested_Rule",parent_rule_heading) if rule_var.get('Nested_Rule') else None,
-                Flow_for_True  = rule_var.get('Flow_for_True', True),
-                Flow_for_False = rule_var.get('Flow_for_False', False),
-
-                Flow_exception_True = Flow_exception(
-                   Exception_rule =  Logical_OR_NestedRule(Flow_exception_True_Flow_rule) if Flow_exception_True_Flow_rule else None,
-                   Condition_to_proceed =  Flow_exception_True_Condition_to_proceed if Flow_exception_True_Condition_to_proceed else False,
-                   Remark = Flow_exception_True_Remark if Flow_exception_True_Remark else ""
-                  ),
-                Flow_exception_False = Flow_exception(
-                   Exception_rule =  Logical_OR_NestedRule(Flow_exception_False_Flow_rule) if Flow_exception_False_Flow_rule else False,
-                   Condition_to_proceed = Flow_exception_False_Condition_to_proceed if Flow_exception_False_Condition_to_proceed else None,
-                   Remark = Flow_exception_False_Remark if Flow_exception_False_Remark else ""
-                  ),
-                
-                logical_operator=rule_var.get('logical_operator', None),
-                Logical_Rule   =   Logical_OR_NestedRule(rule_var['Logical_Rule'],"Logical_Rule",parent_rule_heading) if rule_var.get('Logical_Rule') else None,
-                Remark         =   rule_var.get('Remark', "")
-        )
-    except KeyError as e:
-        raise ValueError(f"Missing required key {e} in rule data: {rule_var}")
-    except Exception as e:
-        raise ValueError(f"An error occurred while creating a rule: {e}")
-    
 def convert_value(value, Field_Type):
     if Field_Type == 0 or Field_Type == '0':  # bool
         return bool(value)
@@ -95,92 +32,6 @@ def convert_value(value, Field_Type):
     else:
         raise ValueError(f"Unknown Field_Type: {Field_Type}")
 
-
-def evaluate(self, data):
-    """
-        Evaluates the rule against provided data.
-
-        Args:
-            data (dict): The data dictionary containing values to be evaluated against the rule.
-
-        Returns:
-            bool: The result of the rule evaluation (True or False).
-    """
-
-    nested_remark = ""
-    if self.Rule_header not in data:
-        raise ValueError(f"Header {self.Rule_header} not found in data.")
-    
-    actual_value = data[self.Rule_header]
-    
-    # Handle date comparison
-    if isinstance(actual_value, datetime) or isinstance(self.Rule_value, datetime):
-        self.Rule_value = datetime.strptime(self.Rule_value, "%d/%m/%Y").date() if isinstance(self.Rule_value, str) else self.Rule_value
-        actual_value = actual_value.date() if isinstance(actual_value, datetime) else actual_value
-    
-    try:
-        if isinstance(self.Rule_value, str) and self.Rule_value.isdigit():
-            self.Rule_value = int(self.Rule_value)
-        if isinstance(actual_value, str) and actual_value.isdigit():
-            actual_value = int(actual_value)
-    except ValueError:
-        raise ValueError(f"Cannot convert {self.Rule_value} or {actual_value} to int.")
-    
-    # Add additional operator support here
-    if self.Rule_operator == "==":
-        if isinstance(actual_value, str) and isinstance(self.Rule_value, str):
-            result = actual_value.strip().lower() == self.Rule_value.strip().lower()
-        else:
-            result = actual_value == self.Rule_value
-    elif self.Rule_operator == "!=":
-        if isinstance(actual_value, str) and isinstance(self.Rule_value, str):
-            result = actual_value.strip().lower() != self.Rule_value.strip().lower()
-        else:
-            result = actual_value != self.Rule_value
-    elif self.Rule_operator == ">":
-        result = actual_value > self.Rule_value
-    elif self.Rule_operator == "<":
-        result = actual_value < self.Rule_value
-    elif self.Rule_operator == ">=":
-        result = actual_value >= self.Rule_value
-    elif self.Rule_operator == "<=":
-        result = actual_value <= self.Rule_value
-    elif self.Rule_operator == "in":
-        if isinstance(self.Rule_value, str):
-            self.Rule_value = [x.strip().lower() for x in self.Rule_value.split(",")]
-        result = actual_value.strip().lower() in self.Rule_value
-    elif self.Rule_operator == "not in":
-        if isinstance(self.Rule_value, str):
-            self.Rule_value = [x.strip().lower() for x in self.Rule_value.split(",")]
-        result = actual_value.strip().lower() not in self.Rule_value
-    else:
-        raise ValueError(f"Unsupported operator: {self.Rule_operator}")
-    
-    if self.logical_operator:
-        if self.logical_operator == "and":
-            result = result and self.Logical_Rule.evaluate(data)['Return_result']
-        elif self.logical_operator == "or":
-            result = result or self.Logical_Rule.evaluate(data)['Return_result']
-    
-    if result:
-        if self.Flow_Exception_for_True and self.Flow_Exception_for_True.Remark:
-            nested_remark += self.Flow_Exception_for_True.Remark
-        if self.Flow_Exception_for_True and self.Flow_Exception_for_True.Exception_rule:
-            LLO = self.Flow_Exception_for_True.Exception_rule.evaluate(data)
-            nested_remark = nested_remark+" || "+LLO['Remark'] if nested_remark != "" else LLO['Remark']
-            result = LLO['Return_result']
-    else:
-        if self.Flow_Exception_for_False and self.Flow_Exception_for_False.Remark:
-            nested_remark += self.Flow_Exception_for_False.Remark
-        if self.Flow_Exception_for_False and self.Flow_Exception_for_False.Exception_rule:
-            LLO = self.Flow_Exception_for_False.Exception_rule.evaluate(data)
-            nested_remark = nested_remark+" || "+LLO['Remark'] if nested_remark != "" else LLO['Remark']
-            result = LLO['Return_result']        
-    self.Evaluated_result = result
-    return {"Return_result": result, "Remark": nested_remark }
-
-
-
 def  take_decisions(Evaluation_result:bool, True_Condition_to_proceed: Rule, False_Condition_to_proceed):
         """
             Determines the next action based on the evaluated result of the rule.
@@ -196,7 +47,15 @@ def  take_decisions(Evaluation_result:bool, True_Condition_to_proceed: Rule, Fal
    
 
 def evaluate_result(Rule_header, Rule_operator, Rule_value, Data):
-    nested_remark = ""
+    """
+        Evaluates the rule against provided data.
+
+        Args:
+            data (dict): The data dictionary containing values to be evaluated against the rule.
+
+        Returns:
+            bool: The result of the rule evaluation (True or False).
+    """
     if Rule_header not in Data:
         raise ValueError(f"Header {Rule_header} not found in data.")
     
@@ -250,41 +109,65 @@ def evaluate_result(Rule_header, Rule_operator, Rule_value, Data):
 
 
 def parse_nested_rule(Rule_element, Data_for_rule):
+    """
+        Recursively parsing the XMl Rule Element and extracting the required attribues from it. then applying the Data for the specific rules. Handles nested and logical rules.
 
+        Args:
+            rule_var (dict): Dictionary containing rule information.
+            rule_key (str): Unique identifier for the rule.
+            parent_rule_heading (str): The heading of the parent rule, used for reference.
+
+        Returns:
+            Rule: A Rule object created based on the input rule data.
+    """
     ID             =  Rule_element.tag
     Rule_header    =  Rule_element.attrib['reference_field']
     Rule_operator  =  Rule_element.attrib['rule_Operator']
     Field_Type     =  Rule_element.attrib.get('field_Type')
     Rule_value     =  convert_value(Rule_element.attrib['rule_Value'], Field_Type)
-    # Evaluation_result = False  ## The Evaluation will be executed here
     Evaluation_result  = evaluate_result(Rule_header, Rule_operator, Rule_value, Data_for_rule)
+    Is_Evaluating  =  False if Rule_element.attrib.get('is_Evaluating', True) == "false" else True
+    nested_remark = ""
+    Flow_exception_True_Flow_rule, Flow_exception_True_Condition_to_proceed,Flow_exception_True_Remark,Flow_exception_False_Flow_rule, Flow_exception_False_Condition_to_proceed,Flow_exception_False_Remark = None, None, None, None, None, None
+
     if Rule_element.find("Flow_for_True_eval") is not None:
         Flow_Exception_for_True = Rule_element.find("Flow_for_True_eval")
-        if Flow_Exception_for_True.find('Exception_rule'):
-            Flow_exception_True_Flow_rule = Flow_Exception_for_True.find('Exception_rule')
-        if Flow_Exception_for_True.attrib.get('Condition_to_proceed'): 
-            Flow_exception_True_Condition_to_proceed = Flow_Exception_for_True.attrib.get('Condition_to_proceed')
-        if Flow_Exception_for_True.attrib.get('Remark'): 
+        if Flow_Exception_for_True.attrib.get('Condition_to_proceed'):
+            Flow_exception_True_Condition_to_proceed =  True if Flow_Exception_for_True.attrib.get('Condition_to_proceed') == "true" else False
+        if Flow_Exception_for_True.attrib.get('Remark'):
             Flow_exception_True_Remark = Flow_Exception_for_True.attrib.get('Remark')
+        if Evaluation_result:
+            nested_remark += Flow_exception_True_Remark
+        if Evaluation_result and (Flow_Exception_for_True.find('Exception_rule') is not None):
+            Flow_exception_True_Flow_rule = Flow_Exception_for_True.find('Exception_rule')
+            LLO = parse_nested_rule(Flow_exception_True_Flow_rule, Data_for_rule)
+            nested_remark = nested_remark+" || "+LLO['Remark'] if nested_remark != "" else LLO['Remark']
+            Evaluation_result = LLO['Return_result']
+
 
     if Rule_element.find("Flow_for_False_eval") is not None:
         Flow_Exception_for_False = Rule_element.find("Flow_for_False_eval")
-        if Flow_Exception_for_False.find('Exception_rule'):
-            Flow_exception_False_Flow_rule = Flow_Exception_for_False.find('Exception_rule')
         if Flow_Exception_for_False.attrib.get('Condition_to_proceed'): 
-            Flow_exception_False_Condition_to_proceed = Flow_Exception_for_False.attrib.get('Condition_to_proceed')
+            Flow_exception_False_Condition_to_proceed =  True if Flow_Exception_for_False.attrib.get('Condition_to_proceed') == "true" else False
         if Flow_Exception_for_False.attrib.get('Remark'): 
             Flow_exception_False_Remark = Flow_Exception_for_False.attrib.get('Remark')
-    
+        if not Evaluation_result:
+            nested_remark += Flow_exception_False_Remark
+        if (not Evaluation_result) and (Flow_Exception_for_False.find('Exception_rule') is not None):
+            Flow_exception_False_Flow_rule = Flow_Exception_for_False.find('Exception_rule')
+            LLO = parse_nested_rule(Flow_exception_False_Flow_rule, Data_for_rule)
+            nested_remark = nested_remark+" || "+LLO['Remark'] if nested_remark != "" else LLO['Remark']
+            Evaluation_result = LLO['Return_result']
 
-    take_decisions(Evaluation_result, Flow_exception_True_Condition_to_proceed, Flow_exception_False_Condition_to_proceed)
-    # evaluating 
-    Is_Evaluating  =  Rule_element.attrib.get('Is_Evaluating', True)
 
+    # evaluating
+    if Is_Evaluating:
+        Evaluation_result =  True if take_decisions(Evaluation_result, Flow_exception_True_Condition_to_proceed, Flow_exception_False_Condition_to_proceed) else False
+    else:
+        Evaluation_result = True
 
-    # print('==> ',Rule_header, Rule_operator, Rule_value)
-    # print("The Rule operator is  ",Rule_operator, Field_Type)
-    return True
+    return {"Return_result": Evaluation_result, "Remark": nested_remark }
+
 
 class Rules_XML_Parser:
     def __init__(self, xml_file_name, xsd_file_name):
@@ -310,10 +193,18 @@ class Rules_XML_Parser:
                     Dict[lender_name][plan_name] = {} 
                     connections = None
                     for rules in  list(plans):# reversed(list(plans)):
-                        rule = parse_nested_rule(rules, Data_for_rule)
-                        print("The Rule Result is :",rule)
-                    break
+                        rule_name = rules.attrib['name']
+                        rule_output = parse_nested_rule(rules, Data_for_rule)
+                        Dict[lender_name][plan_name][rule_name] = rule_output
+                        if not rule_output['Return_result']:
+                            print("Kya hua Result is :",rule_output)
+                            break
+                        print("The Rule Result is :",rule_output)
+                    # break
+                    
                         # connections = Rule_Connection(ID=rules.attrib['name'], Rule=rule, next_Rule=connections)
                     # Dict[lender_name][plan_name] = connections
             # self.lender_rules = Dict
+        print("==="*30)
+        print("The Result Dictionary",Dict)
 
